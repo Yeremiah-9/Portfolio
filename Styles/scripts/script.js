8da0b2baf2db4791aace96bb39f185ae0a939f6e
Yeremiah-9 <djeyremiah.p@gmail.com>
2022-05-26 21:14:02 +0100

    add js

---
+++ Styles/scripts/script.js
@@ -0,0 +1,23 @@
+let make = prompt("Enter your car make")
+let mileage = prompt("Enter your mileage")
+
+
+
+if milage>= 50,000 (ford || bmw) && milage>
+alert ("you need a new car")
+
+
+// You may need to google "javascript IF syntax"
+
+
+//if you drive a ford, OR a bmw AND your milage is more than 50,000 ....
+// alert ("you need a new car")
+
+
+// test with toyota 100,000 miles
+// test with bmw 10,000 miles
+// test with bmw 100,000 miles
+// test with ford 50,000 miles
+
+
+//Extension task - ELSE alert "Your car is OK"
