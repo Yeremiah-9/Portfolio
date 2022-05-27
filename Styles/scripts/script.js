a04e5921f23291b3300b44cb38bdc879e8271d6c
Yeremiah-9 <djeyremiah.p@gmail.com>
2022-05-27 12:13:41 +0100

    changes

--- Styles/scripts/script.js
+++ Styles/scripts/script.js
@@ -3,6 +3,19 @@ let mileage = prompt("Enter your mileage")
 
 
 
+
+'use strict';
+
+// Write a day of week calculator - enter a day number (0 - 6) and display "Sunday is 0  - Saturday" is 6
+
+let dayNum = prompt ("Enter the day number")
+
+if (dayNum ...)
+
+
+else 
+
+
 // You may need to google "javascript IF syntax"
 
 
@@ -17,3 +30,13 @@ let mileage = prompt("Enter your mileage")
 
 
 //Extension task - ELSE alert "Your car is OK"
+
+//Extension task - ELSE alert "Your car is OK"
+
+
+//Extended extension ... 
+//if your car has done less than 10,000 miles - display, "Your car is nearly new"
+//if your car has done 10,000 miles or more display "Your car is getting old"
+//if your car has done 100,000 miles or more display "It's time for a new car"
+
+//Only 1 of the above messages should display
